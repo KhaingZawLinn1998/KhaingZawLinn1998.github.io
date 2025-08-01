6a1739023da6c0c2005ab1ce2204b81f57d59501
Khaing Zaw Linn <kz.linn@mit.com.mm>
2025-08-01 16:53:42 +0630

    c

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3578382048"
+    serviceWorkerVersion: "3022557797"
   }
 });
