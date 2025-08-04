1bd565a530f4ffd7907eff5773785395e4f07a0d
Khaing Zaw Linn <kz.linn@mit.com.mm>
2025-08-04 11:33:12 +0630

    c

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -497,7 +497,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "757145b33a845383af4d35a2f1c51d1f
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "c683795e5c083d99c6229ef4de3b985d",
+"flutter_bootstrap.js": "26fb1795cab1334268fed7c4f72213a0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
